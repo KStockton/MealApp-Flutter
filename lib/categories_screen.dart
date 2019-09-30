8d3c0f85bef1879fec1f4767b5b4c793c14c3d3d
Michael KS <kingstockton@gmail.com>
2019-09-30 17:03:16 -0600

    add new page with router

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -3,28 +3,29 @@ import 'package:flutter/material.dart';
 import './dummy_data.dart';
 import './category_item.dart';
 
+@override
 class CategoriesScreen extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('DeliMeal')
+        title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
-        .map(
-          (categoryData) => CategoryItem(
-            categoryData.title, 
-            categoryData.color,
-          )
-        ).toList(), 
+            .map((categoryData) => CategoryItem(
+                  categoryData.id,
+                  categoryData.title,
+                  categoryData.color,
+                ))
+            .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
-        )
-        ,)
-    ,);
+        ),
+      ),
+    );
   }
 }
